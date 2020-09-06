1e16cacc702bec11aaffc09c13ac9c2cc5890e56
Nathan Buckles <nbuckles@gmail.com>
2020-09-05 23:38:38 -0500

    add support for parsing header extensions and payload padding

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,18 @@
 use crate::rtp::*;
+use crate::rtp_header_extension::*;
 
 extern crate nom;
 use nom::bits::bits;
-use nom::bits::complete::take;
+use nom::bits::complete::take as bits_take;
+use nom::bytes::complete::take as bytes_take;
 use nom::combinator::rest;
+use nom::error::make_error;
+use nom::error::ErrorKind;
+use nom::Err;
+use nom::IResult;
 use nom::multi::count;
 use nom::number::complete::{be_u16, be_u32};
-use nom::{sequence::tuple, IResult};
+use nom::sequence::tuple;
 
 /// Parse an RTP packet
 pub fn parse_rtp_packet(input: &[u8]) -> IResult<&[u8], RtpPacket> {
@@ -22,7 +28,8 @@ pub fn parse_rtp_packet(input: &[u8]) -> IResult<&[u8], RtpPacket> {
     ) = tuple((parse_vpxcc, parse_mpt, be_u16, be_u32, be_u32))(input)?;
 
     let (input, csrc) = parse_csrc(input, csrc_count as usize)?;
-    let (input, payload) = parse_payload(input)?;
+    let (input, header_extension) = parse_header_extension(input, extension)?;
+    let (input, (payload, padding_bytes)) = parse_payload(input, padding)?;
 
     Ok((
         input,
@@ -37,37 +44,78 @@ pub fn parse_rtp_packet(input: &[u8]) -> IResult<&[u8], RtpPacket> {
             timestamp,
             ssrc,
             csrc,
+            header_extension,
             payload,
+            padding_bytes,
         },
     ))
 }
 
 fn parse_vpxcc(input: &[u8]) -> IResult<&[u8], (u8, u8, u8, u8)> {
-    let (input, (v, p, x, cc)) = bits(tuple((
-        take::<_, _, _, (_, _)>(2usize),
-        take::<_, _, _, (_, _)>(1usize),
-        take::<_, _, _, (_, _)>(1usize),
-        take::<_, _, _, (_, _)>(4usize),
-    )))(input)?;
-
-    Ok((input, (v, p, x, cc)))
+    bits(tuple((
+        bits_take::<_, _, _, (_, _)>(2usize),
+        bits_take::<_, _, _, (_, _)>(1usize),
+        bits_take::<_, _, _, (_, _)>(1usize),
+        bits_take::<_, _, _, (_, _)>(4usize),
+    )))(input)
 }
 
 fn parse_mpt(input: &[u8]) -> IResult<&[u8], (u8, u8)> {
-    let (input, (m, pt)) = bits(tuple((
-        take::<_, _, _, (_, _)>(1usize),
-        take::<_, _, _, (_, _)>(7usize),
-    )))(input)?;
-
-    Ok((input, (m, pt)))
+    bits(tuple((
+        bits_take::<_, _, _, (_, _)>(1usize),
+        bits_take::<_, _, _, (_, _)>(7usize),
+    )))(input)
 }
 
 fn parse_csrc(input: &[u8], csrc_count: usize) -> IResult<&[u8], Vec<u32>> {
     count(be_u32, csrc_count)(input)
 }
 
-fn parse_payload(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    rest::<_, (_, _)>(input)
+fn parse_header_extension(
+    input: &[u8],
+    extension: u8,
+) -> IResult<&[u8], Option<RtpPacketHeaderExtension>> {
+    if extension == 0 {
+        Ok((input, None))
+    } else {
+        let (input, (profile, length)) = tuple((be_u16, be_u16))(input)?;
+        let (input, data) = bytes_take(length * 4)(input)?;
+
+        Ok((
+            input,
+            Some(RtpPacketHeaderExtension {
+                profile,
+                length,
+                data,
+            }),
+        ))
+    }
+}
+
+fn parse_payload_no_padding(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+    let (input, payload) = rest::<_, (_, _)>(input)?;
+    Ok((input, (payload, &[])))
+}
+
+fn parse_payload_padding(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+    let num_padding_bytes = input.last().map(|i| *i as usize).unwrap_or(usize::MAX);
+
+    if num_padding_bytes <= input.len() {
+        tuple((
+            bytes_take(input.len() - num_padding_bytes),
+            bytes_take(num_padding_bytes),
+        ))(input)
+    } else {
+        Err(Err::Error(make_error(input, ErrorKind::Eof)))
+    }
+}
+
+fn parse_payload(input: &[u8], padding: u8) -> IResult<&[u8], (&[u8], &[u8])> {
+    if padding == 0x00 {
+        parse_payload_no_padding(input)
+    } else {
+        parse_payload_padding(input)
+    }
 }
 
 #[cfg(test)]
@@ -180,8 +228,97 @@ mod tests {
     }
 
     #[test]
-    fn parse_rtp_packet_no_csrc() {
-        let data = vec!(0x80, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09);
+    fn parse_header_extension_empty_valid() {
+        let input: [u8; 0] = [];
+        let result = parse_header_extension(&input, 0);
+        assert!(result.is_ok());
+
+        let data = result.ok().unwrap();
+        assert!(data.1.is_none());
+    }
+
+    #[test]
+    fn parse_header_extension_empty_invalid() {
+        let input: [u8; 0] = [];
+        let result = parse_header_extension(&input, 1);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn parse_header_extension_non_empty() {
+        let input: [u8; 8] = [0x12, 0x34, 0x00, 0x01, 0x56, 0x78, 0x90, 0xAB];
+        let result = parse_header_extension(&input, 1);
+        assert!(result.is_ok());
+
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+        assert!(data.1.is_some());
+
+        let header_extension = data.1.unwrap();
+        assert_eq!(header_extension.profile, 0x1234);
+        assert_eq!(header_extension.length, 0x0001);
+        assert_eq!(header_extension.data, [0x56, 0x78, 0x90, 0xAB]);
+    }
+
+    #[test]
+    fn parse_payload_no_padding() {
+        let input: [u8; 1] = [0x12];
+        let result = parse_payload(&input, 0);
+        assert!(result.is_ok());
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+
+        let (payload, padding_bytes) = data.1;
+        assert_eq!(payload, [0x12]);
+        assert_eq!(padding_bytes, []);
+    }
+
+    #[test]
+    fn parse_payload_padding_valid() {
+        let input: [u8; 4] = [0x12, 0x00, 0x00, 0x03];
+        let result = parse_payload(&input, 1);
+        assert!(result.is_ok());
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+
+        let (payload, padding_bytes) = data.1;
+        assert_eq!(payload, [0x12]);
+        assert_eq!(padding_bytes, [0x00, 0x00, 0x03]);
+    }
+
+    #[test]
+    fn parse_payload_padding_valid_no_payload() {
+        let input: [u8; 4] = [0x12, 0x00, 0x00, 0x04];
+        let result = parse_payload(&input, 1);
+        assert!(result.is_ok());
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+
+        let (payload, padding_bytes) = data.1;
+        assert_eq!(payload, []);
+        assert_eq!(padding_bytes, [0x12, 0x00, 0x00, 0x04]);
+    }
+
+    #[test]
+    fn parse_payload_padding_invalid() {
+        // number of padding bytes (last value in array) is too large
+        let input: [u8; 4] = [0x12, 0x00, 0x00, 0x05];
+        let result = parse_payload(&input, 1);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn parse_payload_padding_empty() {
+        let input: [u8; 0] = [];
+        let result = parse_payload(&input, 1);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn parse_rtp_packet_no_csrc_no_header_extension() {
+        let data = vec![
+            0x80, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09,
+        ];
         let result = parse_rtp_packet(&data);
 
         assert!(result.is_ok());
@@ -200,12 +337,17 @@ mod tests {
         assert_eq!(packet.timestamp, 0x567890AB);
         assert_eq!(packet.ssrc, 0xCDEFFEDC);
         assert_eq!(packet.csrc, vec![]);
+        assert!(packet.header_extension.is_none());
         assert_eq!(packet.payload, [0xBA, 0x09]);
+        assert_eq!(packet.padding_bytes, []);
     }
 
     #[test]
-    fn parse_rtp_packet_one_csrc() {
-        let data = vec!(0x81, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09, 0x87, 0x65, 0x43);
+    fn parse_rtp_packet_csrc() {
+        let data = vec![
+            0x81, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09,
+            0x87, 0x65, 0x43,
+        ];
         let result = parse_rtp_packet(&data);
 
         assert!(result.is_ok());
@@ -224,6 +366,113 @@ mod tests {
         assert_eq!(packet.timestamp, 0x567890AB);
         assert_eq!(packet.ssrc, 0xCDEFFEDC);
         assert_eq!(packet.csrc, vec![0xBA098765]);
+        assert!(packet.header_extension.is_none());
         assert_eq!(packet.payload, [0x43]);
+        assert_eq!(packet.padding_bytes, []);
+    }
+
+    #[test]
+    fn parse_rtp_packet_header_ext() {
+        let data = vec![
+            0x90, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09,
+            0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11, 0x43,
+        ];
+        let result = parse_rtp_packet(&data);
+
+        assert!(result.is_ok());
+
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+
+        let packet = data.1;
+        assert_eq!(packet.version, 0x02);
+        assert_eq!(packet.padding, 0x00);
+        assert_eq!(packet.extension, 0x01);
+        assert_eq!(packet.csrc_count, 0x00);
+        assert_eq!(packet.marker, 0x00);
+        assert_eq!(packet.payload_type, 0x11);
+        assert_eq!(packet.sequence_number, 0x1234);
+        assert_eq!(packet.timestamp, 0x567890AB);
+        assert_eq!(packet.ssrc, 0xCDEFFEDC);
+        assert_eq!(packet.csrc, vec![]);
+        assert!(packet.header_extension.is_some());
+
+        let header_extension = packet.header_extension.unwrap();
+        assert_eq!(header_extension.profile, 0xBA09);
+        assert_eq!(header_extension.length, 0x0002);
+        assert_eq!(
+            header_extension.data,
+            [0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11]
+        );
+
+        assert_eq!(packet.payload, [0x43]);
+        assert_eq!(packet.padding_bytes, []);
+    }
+
+    #[test]
+    fn parse_rtp_packet_csrc_header_ext() {
+        let data = vec![
+            0x91, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09,
+            0x87, 0x65, 0x43, 0x21, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11,
+            0x43,
+        ];
+        let result = parse_rtp_packet(&data);
+
+        assert!(result.is_ok());
+
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+
+        let packet = data.1;
+        assert_eq!(packet.version, 0x02);
+        assert_eq!(packet.padding, 0x00);
+        assert_eq!(packet.extension, 0x01);
+        assert_eq!(packet.csrc_count, 0x01);
+        assert_eq!(packet.marker, 0x00);
+        assert_eq!(packet.payload_type, 0x11);
+        assert_eq!(packet.sequence_number, 0x1234);
+        assert_eq!(packet.timestamp, 0x567890AB);
+        assert_eq!(packet.ssrc, 0xCDEFFEDC);
+        assert_eq!(packet.csrc, vec![0xBA098765]);
+        assert!(packet.header_extension.is_some());
+
+        let header_extension = packet.header_extension.unwrap();
+        assert_eq!(header_extension.profile, 0x4321);
+        assert_eq!(header_extension.length, 0x0002);
+        assert_eq!(
+            header_extension.data,
+            [0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11]
+        );
+
+        assert_eq!(packet.payload, [0x43]);
+        assert_eq!(packet.padding_bytes, []);
+    }
+
+    #[test]
+    fn parse_rtp_packet_padding() {
+        let data = vec![
+            0xA0, 0x11, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0xFE, 0xDC, 0xBA, 0x09, 0x03
+        ];
+        let result = parse_rtp_packet(&data);
+
+        assert!(result.is_ok());
+
+        let data = result.ok().unwrap();
+        assert_eq!(data.0.len(), 0);
+
+        let packet = data.1;
+        assert_eq!(packet.version, 0x02);
+        assert_eq!(packet.padding, 0x01);
+        assert_eq!(packet.extension, 0x00);
+        assert_eq!(packet.csrc_count, 0x00);
+        assert_eq!(packet.marker, 0x00);
+        assert_eq!(packet.payload_type, 0x11);
+        assert_eq!(packet.sequence_number, 0x1234);
+        assert_eq!(packet.timestamp, 0x567890AB);
+        assert_eq!(packet.ssrc, 0xCDEFFEDC);
+        assert_eq!(packet.csrc, vec![]);
+        assert!(packet.header_extension.is_none());
+        assert_eq!(packet.payload, []);
+        assert_eq!(packet.padding_bytes, [0xBA, 0x09, 0x03]);
     }
 }
--- src/rtp.rs
+++ src/rtp.rs
@@ -1,37 +1,44 @@
+use crate::rtp_header_extension::{RtpPacketHeaderExtension};
 
-// An RTP packet
+/// An RTP packet, https://tools.ietf.org/html/rfc3550#section-5.1
 #[derive(Debug)]
 pub struct RtpPacket<'a> {
-    // version -- 2 bits
+    /// version -- 2 bits
     pub version: u8,
 
-    // padding flag -- 1 bit
+    /// padding flag -- 1 bit
     pub padding: u8,
 
-    // extension flag -- 1 bit
+    /// extension flag -- 1 bit
     pub extension: u8,
 
-    // csrc count -- 4 bits
+    /// csrc count -- 4 bits
     pub csrc_count: u8,
 
-    // marker flag -- 1 bit
+    /// marker flag -- 1 bit
     pub marker: u8,
 
-    // payload type -- 7 bits
+    /// payload type -- 7 bits
     pub payload_type: u8,
 
-    // sequence number -- 2 bytes
+    /// sequence number -- 2 bytes
     pub sequence_number: u16,
 
-    // timestamp -- 4 bytes
+    /// timestamp -- 4 bytes
     pub timestamp: u32,
 
-    // synchronization source identifier
+    /// synchronization source identifier
     pub ssrc: u32,
 
-    // contributing source identifiers (0 to 15)
+    /// contributing source identifiers (0 to 15)
     pub csrc: Vec<u32>,
 
-    // payload
+    /// optional header extension
+    pub header_extension: Option<RtpPacketHeaderExtension<'a>>,
+
+    /// payload
     pub payload: &'a [u8],
+
+    /// padding
+    pub padding_bytes: &'a [u8],
 }
