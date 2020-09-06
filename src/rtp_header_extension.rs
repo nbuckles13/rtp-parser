2c01171ad71de53bfd1fa836c111eb2dd12ae3a0
Nathan Buckles <nbuckles@gmail.com>
2020-09-05 23:38:38 -0500

    add support for parsing header extensions and payload padding

---
+++ src/rtp_header_extension.rs
@@ -0,0 +1,13 @@
+
+/// An RTP packet header extension, https://tools.ietf.org/html/rfc3550#section-5.3.1
+#[derive(Debug)]
+pub struct RtpPacketHeaderExtension<'a> {
+    // profile -- 2 bytes
+    pub profile: u16,
+
+    // length (in number of 32 bit words) -- 2 bytes
+    pub length: u16,
+
+    // variable length data -- {length*4} bytes
+    pub data: &'a [u8],
+}
